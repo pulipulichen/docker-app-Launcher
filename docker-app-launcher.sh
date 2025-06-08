9291c30e5bbaf368e65be1671643c4c551c1f405
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-06-08 23:27:30 +0800

    Sun Jun  8 11:27:30 PM CST 2025

--- docker-app-launcher.sh
+++ docker-app-launcher.sh
@@ -504,8 +504,8 @@ cleanup() {
 # =================
 # 設定為同一個使用者 20250608-2259 
 
-# export UID=$(id -u)
-# export GID=$(id -g)
+export DOCKER_UID=$(id -u)
+export DOCKER_GID=$(id -g)
 
 # -----------------
 # 執行指令
